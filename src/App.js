c962ca6d1095126ce6308ea73f6bc336219560ad
Bojan Branovacki <bojan.branovacki@gmail.com>
2022-08-16 13:40:57 +0200

    feat: initial app structure
    create initial app structure and css to make it fully responive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.main}>
+      <section className={styles.contentContainer}>
+        <h1 className={styles.mainTitle}>
+          Currency <br />
+          Converter
+        </h1>
+        <h4 className={styles.subtitle}>Convert more than 160 currencies</h4>
+      </section>
+      <section className={styles.contentContainer}>
+        <div className={styles.whiteContainer}></div>
+      </section>
+    </main>
   );
 }
 
