81c1c75e75e77d5218c397126dc9fb766f103b70
Bojan Branovacki <bojan.branovacki@gmail.com>
2022-08-16 16:44:50 +0200

    feat: add state and await async API call
    add states for from and to currencies, currency data, rate, loading
    and amount. Write function to get data from server using API.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import Loading from "./components/Loading";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [currencyData, setCurrencyData] = useState({});
+  const [rate, setRate] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const [from, setFrom] = useState("EUR");
+  const [to, setTo] = useState("RSD");
+
+  //API key
+  const apiKey = "0941a3eb6f37325639f22249";
+
+  const getCurrencies = async () => {
+    const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${from}`;
+    setLoading(true);
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setCurrencyData(data.conversion_rates);
+      setRate(data.conversion_rates[to]);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCurrencies();
+  }, [from]);
+
   return (
     <main className={styles.main}>
       <section className={styles.contentContainer}>
@@ -13,9 +45,11 @@ function App() {
       </section>
       <section className={styles.contentContainer}>
         <div className={styles.whiteContainer}>
-          <Loading />
+          {loading && <Loading />}
+          {!loading && <></>}
         </div>
       </section>
+      {console.log(currencyData)}
     </main>
   );
 }
